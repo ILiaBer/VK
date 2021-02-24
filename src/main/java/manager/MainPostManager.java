09bbe8041289f2a109b9dd1ea3cbb6d9ec1974dd
ILia Berestov <berestov_2000@bk.ru>
2021-02-24 15:27:53 +0300

    third commit

---
+++ src/main/java/manager/MainPostManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import domain.Post;
+
+public class MainPostManager {
+    private PostManager postManager;
+
+
+    public String search() {
+        Post[] posts = postManager.ownerId();
+        return null;
+    }
+    public String delete() {
+        Post[] posts = postManager.postId();
+        return null;
+}
+}
---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,27 @@
+
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] ownerId() {
+        return null;
+    }
+
+    public Post[] query() {
+        return null;
+    }
+
+    public Post[] ownersOnly() {
+        return null;
+    }
+
+    public Post[] offset() {
+        return null;
+    }
+    public Post[] postId() {
+        return null;
+    }
+}
